7ab086fd9bf60250f50ee8aa1a8e26f2be431c33
Aakarsh Sinha <aakarshsak@gmail.com>
2022-10-22 23:13:00 +0530

    Migrating client app from this repo to a seperate repo

--- src/main/java/com/example/hola/controller/MessageController.java
+++ src/main/java/com/example/hola/controller/MessageController.java
@@ -5,11 +5,11 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.hola.model.Message;
 
-@Controller
+@RestController
 public class MessageController {
 
 	@Autowired
